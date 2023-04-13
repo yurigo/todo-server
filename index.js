e36411b674eb5d3620b0d1fda225edf41769e460
Yuri <ygonzoo@gmail.com>
2023-04-13 16:57:36 +0200

    graphql starter deprecated!

--- index.js
+++ index.js
@@ -12,9 +12,39 @@ const usersRouter = require("./routers/users.router.js");
 
 app.use("/users", usersRouter);
 
+
+const typeDefs = require("./typedefs");
+const resolvers = require("./resolvers");
+
+const { graphqlHTTP } = require("express-graphql")
+const { createHandler } = require( 'graphql-http/lib/use/express');
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+
+app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema: schema,
+      graphiql: true,
+    })
+);
+
+// app.use(
+//     "/graphql",
+//     createHandler({
+//       schema: schema,
+//     })
+// );
+
+
 app.all("*", (req, res) => {
   res.status(404).json({ error: "esta no es la página que estabas buscando" });
 });
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
